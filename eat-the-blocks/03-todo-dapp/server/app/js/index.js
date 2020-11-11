b7b5a562946b1a0a5565a9760db0df616acb349b
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-11 16:26:21 -0300

    Adds connection to deployed contract

--- eat-the-blocks/03-todo-dapp/server/app/js/index.js
+++ eat-the-blocks/03-todo-dapp/server/app/js/index.js
@@ -1,3 +1,13 @@
-const web3 = new Web3('ws://localhost:9545');
+import Web3 from 'web3';
+import TruffleContract from '@truffle/contract';
 
-web3.eth.getAccounts(console.log);
+import artifact from '../../../build/contracts/ToDo.json';
+
+const provider = new Web3.providers.HttpProvider('http://localhost:9545');
+
+const abstraction = TruffleContract(artifact);
+abstraction.setProvider(provider);
+
+const network = Object.keys(artifact.networks)[0];
+const address = artifact.networks[network].address;
+console.log(address);
