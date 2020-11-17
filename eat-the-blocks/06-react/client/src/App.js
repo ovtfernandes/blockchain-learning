c48eeb56a9d138b461e6e4f00a2b551d40f8aab5
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-17 20:30:43 -0300

    Adds unboxed code

--- eat-the-blocks/06-react/client/src/App.js
+++ eat-the-blocks/06-react/client/src/App.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import getWeb3 from "./getWeb3";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
 
 function App() {
-    return (
-        <h1>React Dapp</h1>
-    );
+    const [storageValue, setStorageValue] = useState(0);
+    const [web3, setWeb3] = useState(null);
+    const [accounts, setAccounts] = useState(null);
+    const [contract, setContract] = useState(null);
+
+    useEffect(() => {
+        (async function() {
+            try {
+                const web3Instance = await getWeb3();
+          
+                const accountsResponse = await web3Instance.eth.getAccounts();
+          
+                const networkId = await web3Instance.eth.net.getId();
+                const deployedNetwork = SimpleStorageContract.networks[networkId];
+                const instance = new web3Instance.eth.Contract(
+                  SimpleStorageContract.abi,
+                  deployedNetwork && deployedNetwork.address,
+                );
+                console.log(instance);
+          
+                await instance.methods.set(5).send({ from: accountsResponse[0] });
+
+                const response = await instance.methods.get().call();
+
+                setWeb3(web3Instance);
+                setAccounts(accountsResponse);
+                setContract(instance);
+                setStorageValue(response);
+            } catch (error) {
+                alert(
+                    'Failed to load web3, accounts, or contract. Check console for details.',
+                );
+                console.error(error);
+            }
+        })();
+    }, []);
+
+    return !web3
+        ? (
+            <div>Loading Web3, accounts, and contract...</div>
+        )
+        : (
+            <div className="App">
+                <h1>Smart Contract Example</h1>
+                <p>
+                    If your contracts compiled and migrated successfully, below will show
+                    a stored value of 5 (by default).
+                </p>
+                <p>
+                    Try changing the value stored on <strong>line 27</strong> of App.js.
+                </p>
+                <div>The stored value is: {storageValue}</div>
+            </div>
+        );
 }
 
 export default App;
