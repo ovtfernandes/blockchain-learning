a972de9fd225b876ce52b3fde4eb76c783437cec
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-23 21:04:46 -0300

    Adds deploy to ropsten

--- dapp-university/01-election/truffle-config.js
+++ dapp-university/01-election/truffle-config.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const infuraKey = process.env.APP_TOKEN;
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   contracts_build_directory: './src/contracts',
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,8 +14,12 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,
+    },
     develop: {
       port: 9545
-    }
+    },
   }
 };
