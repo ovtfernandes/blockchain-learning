5f19f4a94ccb2333c06c1cb81122a4edbe46dc62
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-24 16:13:33 -0300

    Adds code for uploading image to ipfs

--- dapp-university/03-fully-decentralized/src/App.js
+++ dapp-university/03-fully-decentralized/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
+import ipfsClient from 'ipfs-http-client';
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
 function App() {
     const [buffer, setBuffer] = useState(null);
+    const [memePath, setMemePath] = useState('QmPL8ZSiukWKs2DKTL5bogqeVKdbpv1rZSUyChQTTEE9cU');
 
     function captureFile(event) {
         event.preventDefault();
@@ -13,24 +17,26 @@ function App() {
         };
     }
 
-    function onSubmit(event) {
+    async function onSubmit(event) {
         event.preventDefault();
+        const result = await ipfs.add(buffer);
+        setMemePath(result.path);
     }
 
     return (
         <div>
             <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                <a
+                <span
                     className="navbar-brand col-sm-3 col-md-2 mr-0"
                 >
                     Meme of the Day
-                </a>
+                </span>
             </nav>
             <div className="container-fluid mt-5">
                 <div className="row">
                     <main role="main" className="col-lg-12 d-flex text-center">
                         <div className="content mr-auto ml-auto">
-                            {meme && <img src={meme} alt="meme" />}
+                            {memePath && <img width="100%" src={`https://gateway.ipfs.io/ipfs/${memePath}`} alt="meme" />}
                             <h2>Change Meme</h2>
                             <form onSubmit={onSubmit}>
                                 <input type="file" onChange={captureFile} />
